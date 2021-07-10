c917bd008de808473b303c9c3d30799e01f81dcb
Ashutosh Kumar <asutoshkumar14061997@gmail.com>
2021-07-10 15:40:22 +0530

    Modified redirect

--- mernfrontend/src/App.js
+++ mernfrontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { Route,Redirect,Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Bodyfit from './components/Body/Bodyfit';
@@ -18,7 +18,6 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <Switch>
       <Route exact path='/'>
         <Home />
       </Route>
@@ -43,8 +42,6 @@ const App = () => {
       <Route exact path='/verified'>
         <Verified/>
       </Route>
-      <Redirect to='/'/>
-      </Switch>
     </>
   )
 };
