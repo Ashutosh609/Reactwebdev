afc989b06b3f7d704524ce33ee4293916f3be645
Ashutosh Kumar <asutoshkumar14061997@gmail.com>
2021-07-10 16:45:26 +0530

    fixed refresh

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV == "production"){
     app.use(express.static("mernfrontend/build"));
     const path = require('path');
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'mernfrontend',"build,'index.html"));
+        res.sendFile(path.resolve(__dirname,'mernfrontend',"build","index.html"));
     })
 };
 
